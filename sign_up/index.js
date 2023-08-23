7ce9dd1d126fbb97e6c164c2b835e50ec3a9150d
Ankitoo7-droid <smpratik888@gmail.com>
2023-08-23 10:00:30 +0530

    updated backen api

--- sign_up/index.js
+++ sign_up/index.js
@@ -39,16 +39,39 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/signup.html');
 });
 
-app.post('/signup', (req, res) => {
-  const { username, email, password } = req.body;
-  const insertQuery = `INSERT INTO sign_in (username, email, password) VALUES (?, ?, ?)`;
+// app.post('/signup', (req, res) => {
+//   const { username, email, password } = req.body;
+//   const insertQuery = `INSERT INTO sign_in (username, email, password) VALUES (?, ?, ?)`;
   
-  connection.query(insertQuery, [username, email, password], (err, result) => {
+//   connection.query(insertQuery, [username, email, password], (err, result) => {
+//     if (err) {
+//       console.error('Database insert error:', err.message);
+//       res.status(500).send('Error signing up.');
+//     } else {
+//       res.send('Signed up successfully!');
+//     }
+//   });
+// });
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const selectQuery = `SELECT * FROM sign_in WHERE email = ?`;
+
+  connection.query(selectQuery, [email], (err, results) => {
     if (err) {
-      console.error('Database insert error:', err.message);
-      res.status(500).send('Error signing up.');
+      console.error('Database query error:', err.message);
+      res.status(500).send('Error logging in.');
     } else {
-      res.send('Signed up successfully!');
+      if (results.length === 1) {
+        const user = results[0];
+        if (user.password === password) {
+          res.status(200).send('User login successful');
+        } else {
+          res.status(401).send('User not authorized');
+        }
+      } else {
+        res.status(404).send('User not found');
+      }
     }
   });
 });
