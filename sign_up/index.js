475b0d3c6070ce169390a7a26ae5303525036d11
Ankitoo7-droid <smpratik888@gmail.com>
2023-08-23 12:23:53 +0530

    Hash bcrypt

--- sign_up/index.js
+++ sign_up/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const port = 3000;
+const bcrypt = require('bcrypt');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -39,6 +40,29 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/signup.html');
 });
 
+app.post('/signup', (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Hash the password before storing it
+  bcrypt.hash(password, 10, (bcryptErr, hashedPassword) => {
+    if (bcryptErr) {
+      console.error('bcrypt error:', bcryptErr.message);
+      res.status(500).send('Error hashing password.');
+    } else {
+      const insertQuery = `INSERT INTO sign_in (username, email, password) VALUES (?, ?, ?)`;
+      
+      connection.query(insertQuery, [username, email, hashedPassword], (err) => {
+        if (err) {
+          console.error('Database insert error:', err.message);
+          res.status(500).send('Error signing up.');
+        } else {
+          res.send('Signed up successfully!');
+        }
+      });
+    }
+  });
+});
+
 // app.post('/signup', (req, res) => {
 //   const { username, email, password } = req.body;
 //   const insertQuery = `INSERT INTO sign_in (username, email, password) VALUES (?, ?, ?)`;
@@ -53,6 +77,28 @@ app.get('/', (req, res) => {
 //   });
 // });
 
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+  });
+  
+//   app.post('/login', (req, res) => {
+//     const { email, password } = req.body;
+//     const selectQuery = `SELECT * FROM sign_in WHERE email = ? AND password = ?`;
+    
+//     connection.query(selectQuery, [email, password], (err, results) => {
+//       if (err) {
+//         console.error('Database query error:', err.message);
+//         res.status(500).send('Error logging in.');
+//       } else {
+//         if (results.length === 1) {
+//           res.send('Login successful!');
+//         } else {
+//           res.send('Invalid email or password.');
+//         }
+//       }
+//     });
+//   });
+
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
   const selectQuery = `SELECT * FROM sign_in WHERE email = ?`;
@@ -64,11 +110,20 @@ app.post('/login', (req, res) => {
     } else {
       if (results.length === 1) {
         const user = results[0];
-        if (user.password === password) {
-          res.status(200).send('User login successful');
-        } else {
-          res.status(401).send('User not authorized');
-        }
+        
+        // Compare provided password with hashed password
+        bcrypt.compare(password, user.password, (bcryptErr, bcryptResult) => {
+          if (bcryptErr) {
+            console.error('bcrypt error:', bcryptErr.message);
+            res.status(500).send('Error comparing passwords.');
+          } else {
+            if (bcryptResult) {
+              res.status(200).send('User login successful');
+            } else {
+              res.status(401).send('User not authorized');
+            }
+          }
+        });
       } else {
         res.status(404).send('User not found');
       }
@@ -76,27 +131,28 @@ app.post('/login', (req, res) => {
   });
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
-  });
+  // app.post('/login', (req, res) => {
+  //   const { email, password } = req.body;
+  //   const selectQuery = `SELECT * FROM sign_in WHERE email = ?`;
   
-  app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const selectQuery = `SELECT * FROM sign_in WHERE email = ? AND password = ?`;
-    
-    connection.query(selectQuery, [email, password], (err, results) => {
-      if (err) {
-        console.error('Database query error:', err.message);
-        res.status(500).send('Error logging in.');
-      } else {
-        if (results.length === 1) {
-          res.send('Login successful!');
-        } else {
-          res.send('Invalid email or password.');
-        }
-      }
-    });
-  });
+  //   connection.query(selectQuery, [email], (err, results) => {
+  //     if (err) {
+  //       console.error('Database query error:', err.message);
+  //       res.status(500).send('Error logging in.');
+  //     } else {
+  //       if (results.length === 1) {
+  //         const user = results[0];
+  //         if (user.password === password) {
+  //           res.status(200).send('User login successful');
+  //         } else {
+  //           res.status(401).send('User not authorized');
+  //         }
+  //       } else {
+  //         res.status(404).send('User not found');
+  //       }
+  //     }
+  //   });
+  // });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
